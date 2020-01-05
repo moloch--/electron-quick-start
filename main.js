02c6300c8576b50fc16709beabe13ffe7fff1b07
moloch-- <moloch38@gmail.com>
2020-01-05 13:58:30 -0500

    cors bug demo

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, protocol } = require('electron')
 const path = require('path')
+const AppProtocol = require('./app-protocol')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -34,7 +35,19 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  protocol.registerBufferProtocol(AppProtocol.Scheme, AppProtocol.RequestHandler)
+  createWindow()
+})
+
+protocol.registerSchemesAsPrivileged([{
+  scheme: 'app',
+  privileges: { 
+    standard: true,
+    secure: true,
+    corsEnabled: false,
+  }
+}])
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
